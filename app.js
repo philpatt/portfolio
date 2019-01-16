bc4e184f378faa81f692209e362a6592ddf37c95
philpatt <phillip.s.patterson1@gmail.com>
2019-01-15 20:29:53 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var path = require('path')
+var app = express();
+
+app.set('view engine','ejs')
+
+app.set('views', path.join(__dirname, 'templates'));
+//app.use(express.static(path.join(__dirname, 'views')));
+
+app.get('/', function (req, res) {
+    res.render('index.ejs', {name: 'Phil Patterson'} )
+});
+
+app.listen(3000);
