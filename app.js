986f2d798680173c7803ef42aa37c5c2156e50b5
philpatt <phillip.s.patterson1@gmail.com>
2019-01-17 19:19:29 -0800

    added home controller

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ app.set('view engine','ejs')
 app.set('views', path.join(__dirname, 'templates'));
 //app.use(express.static(path.join(__dirname, 'views')));
 
-app.get('/', function (req, res) {
-    res.render('index.ejs', {name: 'Phil Patterson'} )
-});
+
+// Set Controllers
+app.use('/', require('./controllers/home.js')); 
+
+
 
 app.listen(3000);
