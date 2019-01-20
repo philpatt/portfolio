200027bbe1092327b1ca033f5ff4198984841032
philpatt <phillip.s.patterson1@gmail.com>
2019-01-20 13:05:25 -0800

    set canvas height width

--- public/js/starfield.js
+++ public/js/starfield.js
@@ -1,26 +1,11 @@
 console.log('starfield.js loaded...')
 
+// create canvas
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
 
-/**
- * Front-End Starfield background using Canvas
- * 
- * To Do:
- * Create canvas
- * Create background color/gradient
- * Create background starfield
- *  - create star class
- *      - x,y,radius,color
- *      - Math.random location [x,y] - 0-canvas.height/width
- *      - Math.random radius [radius] - 
- *      - color - white
- * 
- *  - add prototypes to star -- will need to do the same for planets
- *      - draw - allows them to be viewed on canvas
- *      - update - will update each to create animation
- *      - 
- * 
- * create planets - see mountain tutorial
- * 
- * 
- * Move from frontend to backend display 
- */
+canvas.height = innerHeight;
+canvas.width = innerWidth;
+
+
+console.log(c)
